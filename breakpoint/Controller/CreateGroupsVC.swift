ce6ea06939596a11b7d11e7acfd249be58993bd1
Justin Seymour <justin@3wmholdings.com>
2017-10-13 10:14:37 +0200

    End of lesson 151

--- breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -21,13 +21,35 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func dobeBtnWasPressed(_ sender: Any) {
     }
     
     @IBOutlet weak var closeBtnWasPressed: UIButton!
+    
+}
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell()}
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: "just@juts.com", isSelected: true)
+        
+        return cell
+    }
+    
     
 }
---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,34 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Justin Seymour on 2017/10/13.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected:Bool) {
+        self.profileImage.image = image
+        self.emailLbl.text = email
+        
+        if isSelected {
+            checkImage.isHidden = false
+        } else {
+            checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
